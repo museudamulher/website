a00de416112342ef8dd520f51c797bb2b0a261d2
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-01-17 20:52:23 +0000

    Changed wording

--- frontend/components/HomepageHistory/HomepageHistory.js
+++ frontend/components/HomepageHistory/HomepageHistory.js
@@ -14,12 +14,12 @@ export default function HomepageHistory() {
         <div className={styles.titlesPt}>
           <p>Nasceu a 8 de Março de 2016.</p>
           <p>Faz parte de uma grande família espalhada pelo mundo.</p>
-          <p>Por enquanto é o único a falar português.</p>
+          <p>E fala português.</p>
         </div>
         <div className={styles.titlesEn}>
           <p>Born on March 8 2016.</p>
           <p>We are part of a large family spread around the world.</p>
-          <p>So far we are the only Portuguese speaking museum.</p>
+          <p>And speaks portuguese.</p>
         </div>
       </div>
       <Link className={styles.badge} href="https://iawm.international/about-us/womens-museums/museums-list/" target="_blank">
