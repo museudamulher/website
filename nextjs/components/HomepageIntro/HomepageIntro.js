92f6c06046d4679af1d386ce1d8c0fba45797ffc
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-03-13 22:40:02 +0000

    Updated wording

--- nextjs/components/HomepageIntro/HomepageIntro.js
+++ nextjs/components/HomepageIntro/HomepageIntro.js
@@ -27,7 +27,7 @@ export default function HomepageIntro() {
       </div>
       <div className={styles.links}>
         <p className={styles.linkPt}>
-          Pode consultar os Estatutos desta instituição e outros documentos <Link href="/documents">aqui</Link>.
+          Pode consultar os Estatutos e outros documentos desta instituição <Link href="/documents">aqui</Link>.
         </p>
         <p className={styles.linkEn}>
           Our legal statutes and other documents can be found <Link href="/documents">here</Link>.
